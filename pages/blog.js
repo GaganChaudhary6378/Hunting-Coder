03a6a6fa3083ef73869d1faeb13bda5f880f1327
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-02-22 18:18:30 +0530

    working on page routing

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,24 @@
 import React from "react";
+import styles from "@/styles/Blog.module.css";
+import Link from "next/link";
 
-export default function(){
-    return (
-        <div>
-            This is my blog page.
-        </div>
-    )
+ 
+export default function () {
+  return (
+    <div className={styles.container}>
+      <h2>Popular Blogs</h2>
+      <div className={styles.mainContent}>
+        <Link style={{textDecoration:'none'}} href="/blogpost/How to learn js"><h3 className={styles.heading1}>How to learn JS in 2022</h3></Link>
+        <p>JavaScript is a language used to design logic for the web</p>
+      </div>
+      <div className={styles.mainContent}>
+        <h3>How to learn JS in 2022</h3>
+        <p>JavaScript is a language used to design logic for the web</p>
+      </div>
+      <div className={styles.mainContent}>
+        <h3>How to learn JS in 2022</h3>
+        <p>JavaScript is a language used to design logic for the web</p>
+      </div>
+    </div>
+  );
 }
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,12 +1,17 @@
 import React, { use } from "react";
 import { useRouter } from "next/router";
+import styles from "@/styles/BlogPost.module.css";
 
 export default function(){
     const router=useRouter();
     const {slug}=router.query;
     return (
-        <div>
-            {slug}
+        <div className={styles.container}>
+            <h1>Title of the page {slug}</h1>
+            <hr/>
+            <div>
+                Lorem ipsum dolor sit ameet consectetur, adipisicing elit.
+            </div>
         </div>
     )
 }
