e6e86d79214f737c64d1f375ab37bbc13da29a02
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-02-24 17:18:15 +0530

    added features

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,5 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import * as fs from "fs";
+export default async  function handler(req, res) {
+  fs.readdir("blogData", (err, data) => {
+    console.log(data);
+    let allBlogs;
+    data.forEach((item) => {
+      console.log(item)
+      fs.readFile(('blogData/'+item)),(d) => {
+        allBlogs.push(d);
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+      }
+    })
+    res.status(200).json({allBlogs});
+  });
 }
