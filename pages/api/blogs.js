c71b806863c735957b182fe9f46d9286a91e2345
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-02-25 22:55:04 +0530

    created blogs API

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,16 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+
+// HERE WE ARE CREATING OUR OWN API FOR USING
+// THE DATA IN BLOGPOST -> BLOG.JS
+
+// PARSE IS USED TO CONVERT STRING TO OBJECT
+
 import * as fs from "fs";
 export default async  function handler(req, res) {
-  fs.readdir("blogData", (err, data) => {
-    console.log(data);
-    let allBlogs;
-    data.forEach((item) => {
-      console.log(item)
-      fs.readFile(('blogData/'+item)),(d) => {
-        allBlogs.push(d);
-
-      }
-    })
-    res.status(200).json({allBlogs});
-  });
+  let data=await fs.promises.readdir("blogData");
+  let myFile;
+  let allBlogs=[];
+  for(let index=0;index<data.length;index++){
+    const item=data[index];
+    myFile=await fs.promises.readFile(('blogData/'+item),'utf-8')
+    allBlogs.push(JSON.parse(myFile)); 
+  }
+  res.status(200).json({allBlogs});
 }
+  // fs.readdir("blogData", (err, data) => {
+  //   console.log(data);
+  //   let allBlogs;
+  //   data.forEach((item) => {
+  //     console.log(item)
+  //     fs.readFile(('blogData/'+item)),(d) => {
+  //       allBlogs.push(d);
+
+  //     }
+  //   })
+  //   res.status(200).json({allBlogs});
+  // });
--- pages/blog.js
+++ pages/blog.js
@@ -1,24 +1,33 @@
 import React from "react";
 import styles from "@/styles/Blog.module.css";
 import Link from "next/link";
-
  
 export default function () {
+  const [blogs,setBlogs]=React.useState([]);
+  React.useEffect(() => {
+    fetch("http://localhost:3000/api/blogs")
+  .then((response) => response.json())
+  .then((data) =>
+  setBlogs(data.allBlogs)
+  );
+  },[])
+
+  console.log(blogs)
+  const content=blogs.map(item => {
+    return (
+      <div className={styles.mainContent} key={item.title}>
+        <Link style={{textDecoration:'none'}} href={`/blogpost/${item.slug}`}>
+        <h3 className={styles.heading1}>{item.title}</h3>
+        </Link>
+        <p className={styles.cont}>{item.content.substr(0,400)}</p>
+      </div>
+    )
+  })
   return (
     <div className={styles.container}>
       <h2>Popular Blogs</h2>
-      <div className={styles.mainContent}>
-        <Link style={{textDecoration:'none'}} href="/blogpost/How to learn js"><h3 className={styles.heading1}>How to learn JS in 2022</h3></Link>
-        <p>JavaScript is a language used to design logic for the web</p>
-      </div>
-      <div className={styles.mainContent}>
-        <h3>How to learn JS in 2022</h3>
-        <p>JavaScript is a language used to design logic for the web</p>
-      </div>
-      <div className={styles.mainContent}>
-        <h3>How to learn JS in 2022</h3>
-        <p>JavaScript is a language used to design logic for the web</p>
-      </div>
+      {content}
     </div>
   );
+
 }
