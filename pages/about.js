bed37213fbb72c0b9ce42414186297564a52eddc
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-03-01 18:04:01 +0530

    about and contact page added

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,23 @@
 import React from "react";
+import styles from '../styles/about.module.css'
 export default function (){
     return (
-        <div>
-            This is my about component.
+        <div className={styles.container}>
+            <h1>About Hunting Coder</h1>
+            <h2>Introduction</h2>
+            <p className={styles.para}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ullamcorper a urna id molestie. Etiam ultrices, neque vel rhoncus iaculis, augue magna gravida dui, et lacinia libero ligula eget ipsum. Duis ornare tempor metus, in venenatis eros tristique nec. Proin vel tortor nisi. Etiam scelerisque.</p>
+            <h2>Services Offered 👇</h2>
+            <p>We offers the services in : -</p>
+            <ul>
+                <li>Service 1</li>
+                <li>Service 2</li>
+                <li>Service 3</li>
+                <li>Service 4</li>
+                <li>Service 5</li>
+            </ul>
+            <p className={styles.para}>Lorem ipsum dolo sit amet, consectetur adipiscing elit. Cras ullamcorper a urna id molestie. Etiam ultrices, neque vel rhoncus iaculis, augue magna gravida dui, et lacinia libero ligula eget ipsum. Duis ornare tempor metus, in venenatis eros tristique nec. Proin vel tortor nisi. Etiam scelerisque.</p>
+            <h2>Contact Us</h2>
+            <p className={styles.para}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ullamcorper a urna id molestie. Etiam ultrices, neque vel rhoncus iaculis, augue magna gravida dui, et lacinia libero ligula eget ipsum. Duis ornare tempor metus, in venenatis eros tristique nec. Proin vel tortor nisi. Etiam scelerisque.</p>
         </div>
     )
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,106 @@
 import React from "react";
+import styles from "../styles/contact.module.css";
 
 export default function () {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [phone, setPhone] = React.useState("");
+  const [desc, setDesc] = React.useState("");
+
+  function handleClick(e) {
+    e.preventDefault();
+    const data = {name,phone,desc,email};
+
+    fetch("http://localhost:3000/api/postcontact", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        alert("Thanks for contacting us!!")
+        setName('')
+        setEmail('')
+        setPhone('')
+        setDesc('')
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+
+  function handleChange(e) {
+    if (e.target.name == "phone") {
+      setPhone(e.target.value);
+    } else if (e.target.name == "email") {
+      setEmail(e.target.value);
+    } else if (e.target.name == "desc") {
+      setDesc(e.target.value);
+    } else if (e.target.name == "name") {
+      setName(e.target.value);
+    }
+  }
+
   return (
-    <div>
-      <h1>Contact Us</h1>
+    <div className={styles.container}>
+      <h1 className={styles.head1}>Contact Us</h1>
+      <form className={styles.formContainer}>
+        <div class={styles.formGroup}>
+          <label for="name">Enter your name</label>
+          <input
+            type="email"
+            value={name}
+            onChange={handleChange}
+            class="form-control"
+            id="name"
+            name="name"
+            aria-describedby="emailHelp"
+            placeholder="Enter name"
+          />
+        </div>
+        <div class={styles.formGroup}>
+          <label for="email">Email Address</label>
+          <input
+            type="email"
+            value={email}
+            onChange={handleChange}
+            class="form-control"
+            id="email"
+            name="email"
+            placeholder="Email Address"
+          />
+        </div>
+        <div class={styles.formGroup}>
+          <label class="form-check-label" for="exampleCheck1">
+            Enter your phone number
+          </label>
+          <input
+            type="number"
+            onChange={handleChange}
+            value={phone}
+            name="phone"
+            class="form-check-input"
+            id="phone"
+          />
+        </div>
+        <div class={styles.formGroup}>
+          <label for="desc">Desc.</label>
+          <input
+            type="text"
+            value={desc}
+            onChange={handleChange}
+            class="form-control"
+            id="desc"
+            name="desc"
+            placeholder="Description"
+          />
+        </div>
+        <button type="submit" onClick={handleClick} class="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
